4f2075eab02ec065b7e2de2ffe3b4b5570ebf827
huzejun <huzejun1990>
2023-12-30 09:08:32 +0800

    golang开发框架-golang命令行开发框架cobra cobra命令行框架三步搞定命令行开始 1.cobra命令行项目基本结构 2.cobra命令行本地标志与持久标志 3.cobra命令参数及参数验证 2023-12-30 09:08:22

---
+++ cmd/root.go
@@ -0,0 +1,10 @@
+// @Author huzejun 2023/12/30 9:02:00
+package cmd
+
+import "github.com/spf13/cobra"
+
+var rootCmd = &cobra.Command{}
+
+func Execute() {
+
+}
---
+++ main.go
@@ -0,0 +1,6 @@
+// @Author huzejun 2023/12/30 9:02:00
+package main
+
+func main() {
+
+}
